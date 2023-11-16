7aa3496a04fb44cbb2143c6e42f85cba64c011b3
VladTkach <vladand2003@gmail.com>
2023-11-16 16:19:08 +0200

    initial create backend

---
+++ backend/FilmRealm.WebApi/Controllers/SampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmRealm.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SampleController: ControllerBase
+{
+    [HttpGet]
+    public ActionResult<string> Test()
+    {
+        return Ok("Something");
+    }
+}
