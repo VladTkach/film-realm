dfa95a3bef9147bad7f94ea67ea376dc16a6cced
VladTkach <vladand2003@gmail.com>
2023-11-20 18:23:08 +0200

    fixed errors

--- frontend/src/app/shared/components/login/login.component.ts
+++ frontend/src/app/shared/components/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent implements OnInit {
 
     this.authService.login(user)
       .subscribe({
-        next: (user) => {
+        next: () => {
           this.closeModal();
         }
       })
